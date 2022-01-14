e10dfa7a85256bec2c007eda873b19308779d2c7
Sid Unnithan <sidunnithan@gmail.com>
2022-01-14 15:56:21 -0800

    removing hardhat project create step

--- setup.sh
+++ setup.sh
@@ -6,6 +6,3 @@ npm install --save-dev hardhat
 npm install --save-dev @nomiclabs/hardhat-waffle ethereum-waffle chai @nomiclabs/hardhat-ethers ethers
 
 npm install @openzeppelin/contracts
-
-# run hardhat
-npx hardhat
